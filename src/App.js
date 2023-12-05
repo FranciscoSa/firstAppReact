398b517fec21d9a964f9c026c9bcdd5e5ee1fbe8
Francisco Santana Cardoso <fsantanagy@gmail.com>
2023-12-05 18:36:02 -0300

    Teste com state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
+import { useState } from "react";
+import Name from "./components/Name";
+
+function App(){
+  const [dinamicName, setdinamicName] = useState("Nome dinamico")
+
+  function changeDinamicName(nome){
+    setdinamicName(nome)
+  }
+
   return (
     <div>
-      <h1>Bem vindo</h1>
+      <h1>First App</h1>
+      <h2>{dinamicName}</h2>
+      <button onClick={() => changeDinamicName("Francisco Cardoso")}>Mudar Nome Dinamico</button>
+      <br/>
+      <Name name="Nome estatico"/>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Name.js
@@ -0,0 +1,7 @@
+function Name({name}){
+    return(
+        <h2>Bem vindo : {name}</h2>
+    );
+}
+
+export default Name;
